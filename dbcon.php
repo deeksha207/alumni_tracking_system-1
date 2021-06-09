d86728705bf4ac925479ae41578f4a923f65a32c
Deeksha Gupta <54803390+deeksha207@users.noreply.github.com>
2021-06-09 18:50:19 +0530

    add remote connection

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,8 @@
 <?php
-$db=mysqli_connect("localhost","root","","sih20");
+//db con
+//$db=mysqli_connect("localhost","root","","sih20");
+//remote db
+$db=mysqli_connect("remotemysql.com","RZNL20qjoC","URV1T4P99G","RZNL20qjoC");
  if(!$db)
    echo "<script> alert('Connection Failed') </script>";  
 ?>
